e60f794122f3e08e294735dde4f35505a7d2710d
Marco L <mloiodice13@gmail.com>
2016-03-03 16:20:32 +0100

    add class and functions for ChecklistsViewController for todolist

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,20 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by iem on 03/03/2016.
+//  Copyright © 2016 loiodice. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    
+    var text: String
+    var checked: Bool
+    
+    init(text: String, checked: Bool = false){
+        self.text = text
+        self.checked = checked
+    }
+}
---
+++ Checklists/ChecklistsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by iem on 03/03/2016.
+//  Copyright © 2016 loiodice. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistsViewController: UITableViewController {
+
+    var listNote = [ChecklistItem]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var one :ChecklistItem = ChecklistItem(text: "Toto")
+        var two :ChecklistItem = ChecklistItem(text: "Titi", checked: true)
+        
+        
+        
+        listNote.append(one)
+        listNote.append(two)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return listNote.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: cellForRowAtIndexPath)
+        cell.textLabel!.text = listNote[cellForRowAtIndexPath.row].text
+        return cell
+    }
+
+
+}
