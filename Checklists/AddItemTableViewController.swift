c877e814261cbe7ea9698aacb773c6077e686ab0
Marco L <mloiodice13@gmail.com>
2016-03-10 13:39:54 +0100

    Steps 15 to 19
    add new interface for AddItem, begin of implemented functions

---
+++ Checklists/AddItemTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddItemTableViewController.swift
+//  Checklists
+//
+//  Created by iem on 10/03/2016.
+//  Copyright © 2016 loiodice. All rights reserved.
+//
+
+import UIKit
+
+class AddItemTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
